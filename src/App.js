a95a5241ef3dada99db3e821d539efe2fa0af01d
Dillorom <dabdulloeva@gmail.com>
2018-11-09 21:18:12 -0800

    dynamically change the user input, remove char by turning string into an array

--- src/App.js
+++ src/App.js
@@ -14,15 +14,26 @@ class App extends Component {
     })
 
   }
+  deleteHandler = (index) => {
+    const text = this.state.userInput.split('')
+    text.splice(index, 1)
+    const updatedText = text.join('')
+    this.setState({
+      userInput: updatedText
+    })
+  }
   render() {
     const charList = this.state.userInput.split('').map((ch, index) => {
-      return <Char character={ch} key={index}/>
+      return <Char character={ch}
+      key={index} 
+      clicked={() => this.deleteHandler(index)}/>
     })
   
     return (
       <div className="App">
         <input type="text" onChange={this.onChangeHandler} value={this.state.userInput}/>
         <Validation inputLength={this.state.userInput.length}/>
+        <p>{this.state.userInput}</p>
         {charList}
       </div>
     );
